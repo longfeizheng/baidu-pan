8e0d5c08e5018ffbb0098e02aefc852fb9edd656
zhenglongfei3 <zhenglongfei3@jd.com>
2021-04-25 21:11:40 +0800

    dev

--- src/main/java/com/niocoder/BaiDuPan.java
+++ src/main/java/com/niocoder/BaiDuPan.java
@@ -1,7 +1,6 @@
 package com.niocoder;
 
 import com.alibaba.fastjson.JSON;
-import com.sun.deploy.net.URLEncoder;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -13,6 +12,7 @@ import java.io.File;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
